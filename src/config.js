7d5088f0fa9362e6dd7629b642d33d129dabc33a
redwoodcodeacademy <info@redwoodcodeacademy.com>
2020-07-08 16:27:12 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "paul-epps-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://p0pidgyawj.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_djX0jnfTR",
+        APP_CLIENT_ID: "4s7r9o2ncop8m7sbkem9tnqbap",
+        IDENTITY_POOL_ID: "us-east-1:d2e00964-f435-4466-b2ca-f82a50494a31"
+    }
+};
